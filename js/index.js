8edef29c9002066624a98f7a1adaa56f40f14a65
john <john@spinningplates.org>
2019-09-04 15:33:40 -0700

    mvp complete

--- js/index.js
+++ js/index.js
@@ -45,3 +45,81 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById('logo-img');
 logo.setAttribute('src', siteContent['nav']['img-src']);
+
+const navItem = document.querySelectorAll('nav a');
+navItem[0].textContent = siteContent.nav['nav-item-1'];
+navItem[1].textContent = siteContent.nav['nav-item-2'];
+navItem[2].textContent = siteContent.nav['nav-item-3'];
+navItem[3].textContent = siteContent.nav['nav-item-4'];
+navItem[4].textContent = siteContent.nav['nav-item-5'];
+navItem[5].textContent = siteContent.nav['nav-item-6'];
+
+const ctaH1 = document.querySelector('.cta .cta-text h1');
+ctaH1.textContent = siteContent.cta.h1;
+
+const ctaButton = document.querySelector('.cta .cta-text button');
+ctaButton.textContent = siteContent.cta.button;
+
+const ctaImg = document.getElementById('cta-img');
+ctaImg.setAttribute('src', siteContent.cta['img-src']);
+
+const topH4 = document.querySelectorAll(
+  '.main-content .top-content .text-content h4'
+);
+topH4[0].textContent = siteContent['main-content']['features-h4'];
+topH4[1].textContent = siteContent['main-content']['about-h4'];
+
+const topP = document.querySelectorAll(
+  '.main-content .top-content .text-content p'
+);
+topP[0].textContent = siteContent['main-content']['features-content'];
+topP[1].textContent = siteContent['main-content']['about-content'];
+
+const middleImg = document.getElementById('middle-img');
+middleImg.setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+const bottomContent = document.querySelectorAll(
+  '.main-content .bottom-content .text-content'
+);
+bottomContent[0].firstElementChild.textContent =
+  siteContent['main-content']['services-h4'];
+bottomContent[0].lastElementChild.textContent =
+  siteContent['main-content']['services-content'];
+bottomContent[1].firstElementChild.textContent =
+  siteContent['main-content']['product-h4'];
+bottomContent[1].lastElementChild.textContent =
+  siteContent['main-content']['product-content'];
+bottomContent[2].firstElementChild.textContent =
+  siteContent['main-content']['vision-h4'];
+bottomContent[2].lastElementChild.textContent =
+  siteContent['main-content']['vision-content'];
+
+const contactH4 = document.querySelector('.contact h4');
+contactH4.textContent = siteContent.contact['contact-h4'];
+
+const contactP = document.querySelectorAll('.contact p');
+contactP[0].textContent = siteContent.contact.address;
+contactP[1].textContent = siteContent.contact.phone;
+contactP[2].textContent = siteContent.contact.email;
+
+const footer = document.querySelector('footer p');
+footer.textContent = siteContent.footer.copyright;
+
+// Task 3: Add new content
+
+// Change the color of the navigation text to be green
+console.log(navItem);
+navItem.forEach(a => (a.style.color = 'green'));
+
+// Utilize .appendChild() and .prepend() to add two new items to the navigation system. You can call them whatever you want.
+const nav = document.querySelector('nav');
+
+const a7 = document.createElement('a');
+a7.textContent = 'Help';
+a7.style.color = 'Green';
+nav.appendChild(a7)
+
+const a8 = document.createElement('a');
+a8.textContent = 'Login';
+a8.style.color = 'Blue';
+nav.appendChild(a8);
