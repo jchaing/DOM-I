85bff88635b20675160c32a212f8d967d40ff690
john <john@spinningplates.org>
2019-09-07 04:24:50 -0700

    mvp timer - counts to ten and stops

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -4,40 +4,35 @@ const msHundreds = document.getElementById('msHundreds');
 const msTens = document.getElementById('msTens');
 
 let secondsCounter = 1;
-let msHundredsCounter = 1;
-let msCounter = 1;
+let msHundredsCounter = 0;
+let msTensCounter = 0;
 
 const timerTenSeconds = setInterval(() => (secondTens.textContent = 1), 10000);
 
-const timerSeconds = setInterval(
+const timerOneSeconds = setInterval(
   () => (secondOnes.textContent = secondsCounter++),
   1000
 );
 
-const timerMsHundred = setInterval(() => {
-  msHundreds.textContent = msHundredsCounter++;
-  if (msHundredsCounter === 9) {
-    msHundreds.textContent = 0;
-  }
-}, 100);
-
-const timerMsTens = setInterval(() => {
-  msTens.textContent = msCounter++;
-  if (msCounter === 10) {
-    msTens.textContent = 0;
+const timerMS = setInterval(() => {
+  if (msTensCounter < 10) {
+    msTens.textContent = msTensCounter++;
+    if (msTensCounter === 9 && msHundredsCounter < 10) {
+      msTensCounter = 0;
+      msHundreds.textContent = msHundredsCounter++;
+      if (msHundredsCounter === 9 && secondsCounter < 10) {
+        msHundredsCounter = 0;
+      }
+    }
   }
 }, 10);
 
 setTimeout(() => {
   clearInterval(timerTenSeconds);
-  clearInterval(timerSeconds);
-  clearInterval(timerMsHundred);
-  clearInterval(timerMsTens);
+  clearInterval(timerOneSeconds);
+  clearInterval(timerMS);
+  secondTens.textContent = 1;
   secondOnes.textContent = 0;
   msHundreds.textContent = 0;
   msTens.textContent = 0;
 }, 10000);
-
-setTimeout(() => {
-  clearInterval;
-});
